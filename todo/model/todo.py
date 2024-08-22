b18a07ad2907bd595a8b47be58757ea80c09b319
santiagouzi <santiagoh8525@gmail.com>
2024-08-22 13:03:35 -0500

    create class todobook , metodo inicializador, metodo add_todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -15,3 +15,13 @@ class Todo:
 
     def __str__(self) -> str:
         return f"{self.code_id} - {self.title}"
+
+class TodoBook:
+    def __init__(self) -> None:
+        self.todos: Dict[int, Todo] = {}
+
+    def add_todo(self, title: str, description: str) -> int:
+        new_id = len(self.todos) + 1
+        new_todo = Todo(code_id=new_id, title=title, description=description)
+        self.todos[new_id] = new_todo
+        return new_id
