4ececc4eca64c34b00af790ad1451e69c521d0d0
santiagouzi <santiagoh8525@gmail.com>
2024-08-22 12:52:25 -0500

    creation class todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,17 @@
-# TODO: Add code here
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str) -> None:
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: List[str] = []
+
+    def mark_completed(self) -> None:
+        self.completed = True
+
+    def add_tag(self, tag: str) -> None:
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"{self.code_id} - {self.title}"
